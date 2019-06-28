445431fcaa6eb8af2d19734a909008e434ec0361
Travis CI <travis@travis-ci.org>
2019-06-28 19:38:00 +0000

    Deploy code docs to GitHub Pages Travis build: 15
    Commit: 9c910ebae1cf6c2093ad6e3f674732880b5404c2

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['adafruit_5fkeypad',['Adafruit_Keypad',['../class_adafruit___keypad.html',1,'Adafruit_Keypad'],['../class_adafruit___keypad.html#abce955bad630a216fcaf9e8bc964246f',1,'Adafruit_Keypad::Adafruit_Keypad()']]],
-  ['available',['available',['../class_adafruit___keypad.html#abc04c159b35914704c12f0e975c82a9e',1,'Adafruit_Keypad']]],
-  ['adafruit_20keypad_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fkeypad_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fkeypad_29',['Adafruit Keypad Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_Keypad.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_Keypad)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['available',['available',['../class_adafruit___keypad.html#abc04c159b35914704c12f0e975c82a9e',1,'Adafruit_Keypad']]]
 ];
